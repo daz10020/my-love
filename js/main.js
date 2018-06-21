cef12a04e29575948e0765a11b1f2cc0bad271e1
daz10020 <18341261967@163.com>
2018-06-21 23:03:55 +0800

    s

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function() {
+	
+	'use strict';
+
+	// For the delete button
+	var quttonDelete = Qutton.getInstance($('#qutton_delete'));
+	quttonDelete.init({
+		icon : './images/icon_comment.png',
+		backgroundColor : "rgb(241,130,65)"
+	});
+
+})();
